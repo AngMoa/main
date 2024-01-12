725e4f8402100aed6e5fd18fab2fff38c9e6a08e
shiningKyung <nkyou15@gmail.com>
2024-01-12 15:15:01 +0900

    Feat: 로그인 기능 구현

--- frontend/src/components/user/SignInView.jsx
+++ frontend/src/components/user/SignInView.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosInstance from "../../api/axiosInstance";
 
 const SignInView = () => {
+    const [loginInput, setLoginInput] = useState({
+        userId: "",
+        password: "",
+    });
+
+    const handleInputChange = (e) => {
+        setLoginInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+        // console.log(e.target);
+    };
+
+    // const handleLogin = () => {
+    //     onLogin();
+    // };
+
+    const onLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await axiosInstance.get(`/com/login`, {
+                loginInput,
+            });
+            if (res.status === 201) {
+                console.log(res.data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <>
             <h2 className="text-center text-2xl text-gray-700">Sing In</h2>
@@ -10,14 +40,17 @@ const SignInView = () => {
             <form className="max-w-sm mx-auto ">
                 <div className="mb-5">
                     <label
-                        htmlFor="email"
+                        htmlFor="userId"
                         className="block mb-2 text-sm font-medium text-gray-700 dark:text-white"
                     >
                         아이디
                     </label>
                     <input
-                        type="email"
-                        id="email"
+                        type="userId"
+                        id="userId"
+                        name="userId"
+                        value={loginInput.userId}
+                        onChange={handleInputChange}
                         className="border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="아이디"
                         required
@@ -33,12 +66,15 @@ const SignInView = () => {
                     <input
                         type="password"
                         id="password"
+                        name="password"
+                        value={loginInput.password}
+                        onChange={handleInputChange}
                         className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="비밀번호"
                         required
                     />
                 </div>
-                <div className="flex items-start mb-5">
+                {/* <div className="flex items-start mb-5">
                     <div className="flex items-center h-5">
                         <input
                             id="remember"
@@ -54,9 +90,10 @@ const SignInView = () => {
                     >
                         자동로그인
                     </label>
-                </div>
+                </div> */}
                 <button
                     type="submit"
+                    onClick={onLogin}
                     className="text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-12 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                     로그인
