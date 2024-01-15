65d869ed07724d59c5c48b9e72f64e805a83c1fa
SHIN JAEYONG <sls0263@gmail.com>
2024-01-15 15:06:15 +0900

    Add: CSRF 활성화

--- backend/src/main/java/org/backend/web/common/config/SecurityConfig.java
+++ backend/src/main/java/org/backend/web/common/config/SecurityConfig.java
@@ -8,7 +8,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Component;
+
+import static org.hibernate.criterion.Restrictions.and;
 
 @RequiredArgsConstructor
 @Configuration
@@ -24,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()//csrf토큰 비활성화(테스트시 걸어두는게 좋음) 시큐리티는 csrf토큰이 있어야 접근가능함
+//            .csrf().disable()//csrf토큰 비활성화(테스트시 걸어두는게 좋음) 시큐리티는 csrf토큰이 있어야 접근가능함
             .authorizeRequests()
                 .antMatchers("/com/login","/com/join", "/**").permitAll()
                 .anyRequest().authenticated()
@@ -35,9 +38,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .permitAll()
 //                .and()
             .logout()
-                .permitAll();
+                .permitAll()
+                .and()
+            .csrf(csrf -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            );
+
         http.sessionManagement() //중복로그인 제어
-                .maximumSessions(1) //세션 최대 허용 수
-                .maxSessionsPreventsLogin(false); // false이면 중복 로그인하면 이전 로그인이 풀린다.
+            .maximumSessions(1) //세션 최대 허용 수
+            .maxSessionsPreventsLogin(false); // false이면 중복 로그인하면 이전 로그인이 풀린다.
     }
 }
