6b7265d975542b1dd4666516c03f86522732456b
SHIN JAEYONG <sls0263@gmail.com>
2024-01-22 13:32:52 +0900

    Feat: 인증번호 이메일 발송 및 인증번호 확인

---
+++ backend/src/main/java/org/backend/web/common/controller/MailController.java
@@ -0,0 +1,34 @@
+package org.backend.web.common.controller;
+
+import org.backend.web.common.dto.EmailRequestDto;
+import org.backend.web.common.dto.EmailCheckDto;
+import org.backend.web.common.service.MailSendService;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.Check;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class MailController {
+    private final MailSendService mailService;
+
+    @PostMapping("/mailSend")
+    public String mailSend(@RequestBody @Valid EmailRequestDto emailDto) {
+        System.out.println("이메일 인증 이메일 :" + emailDto.getEmail());
+        return mailService.joinEmail(emailDto.getEmail());
+    }
+    @PostMapping("/mailauthCheck")
+    public String AuthCheck(@RequestBody @Valid EmailCheckDto emailCheckDto){
+        boolean Checked=mailService.CheckAuthNum(emailCheckDto.getEmail(),emailCheckDto.getAuthNum());
+        if(Checked){
+            return "ok";
+        }
+        else{
+            throw new NullPointerException("관리자에게 문의해주세요.");
+        }
+    }
+}
