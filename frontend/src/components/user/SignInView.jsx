2c9abfaf88030cbcc5d632cae8b825eaee2e809f
shiningKyung <nkyou15@gmail.com>
2024-01-29 19:41:18 +0900

    Add: 로그인 미 입력란 포커스 및 에러문구 추가, 회원정보 찾기 버튼 추가, 오타 수정

--- frontend/src/components/user/SignInView.jsx
+++ frontend/src/components/user/SignInView.jsx
@@ -1,20 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import axiosInstance from "../../api/axiosInstance";
+import { useAuthStore } from "../../store/store";
 
 const SignInView = () => {
+    const navigate = useNavigate();
+    const idInput = useRef(null);
+    const pwInput = useRef(null);
+
     const [loginInput, setLoginInput] = useState({
         userId: "",
         password: "",
     });
+
+    const { userId, password } = loginInput;
+
+    const [errors, setErrors] = useState({
+        invalid: false,
+        message: "",
+    });
+
+    // zustand로 관리할 로그인 여부
+    const { isLoginState, setIsLoginState } = useAuthStore();
 
     const handleInputChange = (e) => {
         setLoginInput((prev) => ({ ...prev, [e.target.name]: e.target.value }));
         // console.log(e.target);
     };
 
-    // const handleLogin = () => {
-    //     onLogin();
-    // };
+    // 로그인
+    const handleLogin = (e) => {
+        if (userId.length < 1) {
+            idInput.current.focus();
+            setErrors({
+                invalid: true,
+                message: "아이디를 입력해주세요.",
+            });
+            e.preventDefault();
+            return;
+        }
+        if (password.length < 1) {
+            password.current.focus();
+            setErrors({
+                invalid: true,
+                message: "비밀번호를 입력해주세요.",
+            });
+            e.preventDefault();
+            return;
+        }
+        onLogin(e);
+    };
 
     const onLogin = async (e) => {
         e.preventDefault();
@@ -25,7 +61,20 @@ const SignInView = () => {
                 password: loginInput.password,
             });
             if (res.status === 200) {
-                console.log(res.data);
+                // 헤더에 토큰 포함
+                const { token } = res.data;
+                axios.defaults.headers.common["Authorization"] =
+                    `Bearer ${token}`;
+
+                // 토큰 저장
+                localStorage.setItem("accessToken", res.data.accessToken);
+                localStorage.setItem("refreshToken", res.data.refreshToken);
+
+                // 로그인 여부
+                setIsLoginState();
+                console.log("로그인 여부 state>", isLoginState);
+
+                navigate("/");
             }
         } catch (error) {
             console.log(error);
@@ -34,7 +83,7 @@ const SignInView = () => {
 
     return (
         <>
-            <h2 className="text-center text-2xl text-gray-700">Sing In</h2>
+            <h2 className="text-center text-2xl text-gray-700">Sign In</h2>
             <p className="text-center text-gray-500">
                 앵모아 계정을 만들고 로그인해보세요.
             </p>
@@ -50,6 +99,7 @@ const SignInView = () => {
                         type="userId"
                         id="userId"
                         name="userId"
+                        ref={idInput}
                         value={loginInput.userId}
                         onChange={handleInputChange}
                         className="border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -68,12 +118,19 @@ const SignInView = () => {
                         type="password"
                         id="password"
                         name="password"
+                        ref={pwInput}
                         value={loginInput.password}
                         onChange={handleInputChange}
                         className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="비밀번호"
                         required
                     />
+                    <Link
+                        to="/findIdPw"
+                        className="label-text-alt link flex flex-row-reverse text-blue-400"
+                    >
+                        아이디/비밀번호 찾기
+                    </Link>
                 </div>
                 {/* <div className="flex items-start mb-5">
                     <div className="flex items-center h-5">
@@ -92,9 +149,12 @@ const SignInView = () => {
                         자동로그인
                     </label>
                 </div> */}
+                {errors.invalid ? (
+                    <p className="text-red-500">{errors.message}</p>
+                ) : null}
                 <button
                     type="submit"
-                    onClick={onLogin}
+                    onClick={handleLogin}
                     className="text-white bg-blue-400 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-12 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                     로그인
