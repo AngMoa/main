66b2d44451b6da5000b84cf12dd84316bd410ff0
shiningKyung <nkyou15@gmail.com>
2024-01-31 18:09:17 +0900

    Add: 회원가입 STEP UI 추가

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -2,19 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Index from "../components/Index";
 import SignIn from "../views/SignIn";
-// import SignUp from "../views/SignUp";
-import RegisterSuccess from "../views/RegisterSuccess";
+import SignUp from "../views/SignUp";
+// import RegisterSuccess from "../views/RegisterSuccess";
 import FindIdPw from "../views/FindIdPw";
-
-import SignUpTest from "../components/user/SignUpTest";
 
 const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/login" element={<SignIn />} />
-            <Route path="/signUp" element={<SignUpTest />} />
-            <Route path="/regSuccess" element={<RegisterSuccess />} />
+            <Route path="/signUp" element={<SignUp />} />
+            {/* <Route path="/regSuccess" element={<RegisterSuccess />} /> */}
             <Route path="/findIdPw" element={<FindIdPw />} />
         </Routes>
     );
